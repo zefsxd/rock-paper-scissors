f0bcd1197ec76bd532f6dfe3c2ebffab8689d1d8
zefsxd <123064630+zefsxd@users.noreply.github.com>
2023-03-02 19:38:35 +1030

    added pseudocode

--- script.js
+++ script.js
@@ -2,3 +2,26 @@
 
 
 
+take input from user
+and store it 
+
+let 1 equal rock 
+let 2 equal paper 
+let 3 equal scissors 
+
+generate a number between 1 and 3
+
+if user picked rock
+and result equal to rock then display a "draw" message 
+else if result equal to paper display a "user lost" message 
+else if result equal to scissors display a "user won" message 
+
+if user picked paper 
+and result equal to rock then display "user won" message
+else if result equal to paper then display a "draw" message 
+else if result equal to scissors then display a "user lost"message 
+
+if user picked scissors 
+and result equal to rock then display "user lost" message
+else if result equal to paper display "user won" message 
+else if result equal to scissors display "draw" message
